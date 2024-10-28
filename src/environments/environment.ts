05910277a060e1b466425e7f303fac1363fac6bd
toritho <mailto:Pjuanpa37@gmail.com>
2024-10-27 21:53:07 -0500

    cambios front

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  urlBase: 'http://localhost:3002',
+  users: 'http://localhost:3002/user',
+  acitivity: 'http://localhost:3002/activity',
+  secretKey: 'K%@Tt48aeAdydX(9p!4q',
+};
+
+
+
+
+
+
+
+
+
+/*
+  urlBase: 'https://back-am-production.up.railway.app',
+  users: 'https://back-am-production.up.railway.app/api/users',
+  acitivity: 'https://back-am-production.up.railway.app/api/activity',
+  secretKey: 'K%@Tt48aeAdydX(9p!4q',
+*/
