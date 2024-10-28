f808075269713303447fc5bf3e97aac10e8eba17
yhiriend <yirien45@gmail.com>
2024-10-27 23:21:19 -0500

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,9 @@
 export const environment = {
-  urlBase: 'http://localhost:3002',
-  users: 'http://localhost:3002/user',
-  acitivity: 'http://localhost:3002/activity',
-  secretKey: 'K%@Tt48aeAdydX(9p!4q',
+  urlBase: 'https://memoriplay-back-production.up.railway.app',
+  users: 'https://memoriplay-back-production.up.railway.app/api/users',
+  acitivity: 'https://memoriplay-back-production.up.railway.app/api/activity',
+  secretKey: 'rN4Z3yn96rJLv!zzB(+q',
 };
-
-
-
-
-
-
-
-
 
 /*
   urlBase: 'https://back-am-production.up.railway.app',
